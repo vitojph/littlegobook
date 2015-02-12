7d86014f63750920c0d316712ef13b89cdcb75a2
Victor Peinado <vitojph@gmail.com>
2015-02-12 22:27:06 +0100

    testing differences between using values and pointers

---
+++ saiyan-pointers.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// a Saiyan struct
+type Saiyan struct {
+    Name string
+    Power int
+}
+
+// a method for the Saiyan struct
+func (s *Saiyan) Super() {
+    s.Power += 10000
+}
+
+
+func main() {
+
+    goku := &Saiyan{"Goku", 9000}
+    goku.Super()
+    fmt.Printf("%s tiene %d de fuerza\n", goku.Name, goku.Power)
+
+}
