ddca6612b1f4738bebdc61487567e18a53e392ee
Victor Peinado <vitojph@gmail.com>
2015-02-12 22:35:09 +0100

    after run go fmt ./... command

--- composition.go
+++ composition.go
@@ -4,48 +4,47 @@ import "fmt"
 
 // let's define a Person
 type Person struct {
-    Name string
+	Name string
 }
 
 // who's able to introduce herself
 func (p *Person) Introduce() {
-    fmt.Printf("Hola, me llamo %s.\n", p.Name)
+	fmt.Printf("Hola, me llamo %s.\n", p.Name)
 }
 
 // composition: one struct into another, letting it access its methods
 type Saiyan struct {
-    *Person
-    Power int
+	*Person
+	Power int
 }
 
+func main() {
 
-func main()  {
+	// un goku
+	persona := &Person{"Manolo"}
 
-    // un goku
-    persona := &Person{"Manolo"}
+	persona.Introduce()
+	fmt.Println(persona)
 
-    persona.Introduce()
-    fmt.Println(persona)
+	// un goku
+	goku := &Saiyan{
+		Person: &Person{"Goku"},
+		Power:  9000,
+	}
+	// un krillin
+	krillin := Saiyan{
+		Person: &Person{"Krillin"},
+		Power:  2000,
+	}
 
-    // un goku
-    goku := &Saiyan{
-        Person: &Person{"Goku"},
-        Power: 9000,
-    }
-    // un krillin
-    krillin := Saiyan{
-        Person: &Person{"Krillin"},
-        Power: 2000,
-    }
+	goku.Power += 100
+	goku.Introduce()
+	fmt.Println(goku)
+	fmt.Printf("%s %d\n", goku.Name, goku.Power)
 
-    goku.Power += 100
-    goku.Introduce()
-    fmt.Println(goku)
-    fmt.Printf("%s %d\n", goku.Name, goku.Power)
-
-    krillin.Power += 100
-    krillin.Introduce()
-    fmt.Println(krillin)
-    fmt.Printf("%s %d\n", krillin.Name, krillin.Power)
+	krillin.Power += 100
+	krillin.Introduce()
+	fmt.Println(krillin)
+	fmt.Printf("%s %d\n", krillin.Name, krillin.Power)
 
 }
--- saiyan-values.go
+++ saiyan-values.go
@@ -4,20 +4,19 @@ import "fmt"
 
 // a Saiyan struct
 type Saiyan struct {
-    Name string
-    Power int
+	Name  string
+	Power int
 }
 
 // a method for the Saiyan struct
 func (s Saiyan) Super() {
-    s.Power += 10000
+	s.Power += 10000
 }
-
 
 func main() {
 
-    goku := Saiyan{"Goku", 9000}
-    goku.Super()
-    fmt.Printf("%s tiene %d de fuerza\n", goku.Name, goku.Power)
+	goku := Saiyan{"Goku", 9000}
+	goku.Super()
+	fmt.Printf("%s tiene %d de fuerza\n", goku.Name, goku.Power)
 
 }
